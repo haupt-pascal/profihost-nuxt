87cd8a46206a007a1d16d9e9cdb03289da91f4d7
Pascal Haupt <pascal.haupt@profihost.ag>
2025-02-26 14:04:07 +0100

    base commit.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+    tailwindcss: {
+        exposeConfig: true,
+        viewer: true,
+    },
 })
---
+++ pages/profihost.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Profihost</h1>
+        <p>Profihost is a German hosting provider.</p>
+    </div>
+</template>
